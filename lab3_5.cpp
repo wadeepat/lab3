f99dd7615af5f3bca21dd059e56810f1dd60f21d
wadeepat <leaw.wadeepat@gmail.com>
2019-12-27 20:56:41 +0700

    finished

---
+++ lab3_5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N,count=0;
+    string text1,text2;
+    cout<<"Enter the first text: ";
+    cin>>text1;
+    cout<<"Enter the second text: ";
+    cin>>text2;
+    cout<<"Enter N: ";
+    cin>>N;
+
+    while (count<N){
+        if (count%2==0){
+            cout<<text1<<" ";
+    }else{
+            cout<<text2<<" ";
+    }
+    count=count+1;
+    }
+
+    return 0;
+}
